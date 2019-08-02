4b3b8eaf6555f2b6c507f597f63dee8012626357
chachakawooka <chachakawooka@googlemail.com>
2019-08-02 01:09:44 +0100

    CREXT needs promise

--- src/extensions/Crext.js
+++ src/extensions/Crext.js
@@ -2,27 +2,25 @@ export default class Crext {
 
 
     authorise(callback) {
-        return callback(window.CREXT.network().then((r) => {
+        return window.CREXT.network().then((r) => {
             if(r.success){
                 return callback(true);
             }
             return callback(false);
-        }
-        ));
+        });
     }
 
     getKey(callback) {
-        return callback(window.CREXT.getKey().then((r) => {
+        return window.CREXT.getKey().then((r) => {
             if (r.success) {
                 return callback(r.result.publicKey);
             }
             return callback(null);;
-        }
-        ));
+        });
     }
 
     balanceGet(key,callback){
-        window.CREXT.balanceGet({key:key},(r) => {
+        window.CREXT.balanceGet({key:key}).then((r) => {
             if(r.success){
                 return callback(r.result.balance);
             }
